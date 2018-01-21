169b5535e0c7d88f48d1a5e7d73a156468f16ef4
吴立民 <1091423040@qq.com>
2018-01-21 15:37:49 +0800

    DrawerLayout+ToolBar整合

--- app/src/main/java/com/dingzhu/appstore/MainActivity.java
+++ app/src/main/java/com/dingzhu/appstore/MainActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
-import com.orhanobut.logger.Logger;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     NavigationView mNavigationView;
     @BindView(R.id.drawerlayout_view)
     DrawerLayout mDrawerlayoutView;
+    @BindView(R.id.tool_bar)
+    Toolbar mToolBar;
     private View headerView;
 
     @Override
@@ -28,27 +30,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mDrawerlayoutView.addDrawerListener(new DrawerLayout.DrawerListener() {
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                Logger.d("onDrawerSlide");
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                Logger.d("onDrawerOpened");
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                Logger.d("onDrawerClosed");
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-                Logger.d("onDrawerStateChanged");
-            }
-        });
+//        mDrawerlayoutView.addDrawerListener(new DrawerLayout.DrawerListener() {
+//            @Override
+//            public void onDrawerSlide(View drawerView, float slideOffset) {
+//                Logger.d("onDrawerSlide");
+//            }
+//
+//            @Override
+//            public void onDrawerOpened(View drawerView) {
+//                Logger.d("onDrawerOpened");
+//            }
+//
+//            @Override
+//            public void onDrawerClosed(View drawerView) {
+//                Logger.d("onDrawerClosed");
+//            }
+//
+//            @Override
+//            public void onDrawerStateChanged(int newState) {
+//                Logger.d("onDrawerStateChanged");
+//            }
+//        });
         headerView = mNavigationView.getHeaderView(0);
         headerView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,17 +64,22 @@ public class MainActivity extends AppCompatActivity {
 
                 switch (item.getItemId()) {
                     case R.id.item_setting:
-                        Toast.makeText(MainActivity.this, "应用设置", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "应用更新", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.item_message:
                         Toast.makeText(MainActivity.this, "消息中心", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.item_search:
-                        Toast.makeText(MainActivity.this, "搜索", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "设置", Toast.LENGTH_SHORT).show();
                         break;
                 }
                 return false;
             }
         });
+        mToolBar.inflateMenu(R.menu.menu_toolbar);
+        ActionBarDrawerToggle barDrawerToggle = new ActionBarDrawerToggle(this, mDrawerlayoutView, mToolBar, R.string.open, R.string.close);
+        barDrawerToggle.syncState();//同步状态
+        mDrawerlayoutView.addDrawerListener(barDrawerToggle);
+
     }
 }
